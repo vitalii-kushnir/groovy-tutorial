8030c09d65c6df95b06f342ba9ae1a304b73987a
Vitalii Kushnir <kusha.apple@gmail.com>
2018-04-12 11:20:46 +0200

    Safe Navigation, Spaceship Operator, and Elvis

---
+++ src/fundamentals/elvis_operator.groovy
@@ -0,0 +1,4 @@
+package fundamentals
+
+displayName = user.name ? user.name : 'Anonymous'
+displayName = user.name ?: 'Anonymous'
---
+++ src/fundamentals/spaceship_operator.groovy
@@ -0,0 +1,9 @@
+package fundamentals
+
+int x = 1
+int y = 2
+int z = 3
+
+println x <=> y
+println y <=> y
+println y <=> z
